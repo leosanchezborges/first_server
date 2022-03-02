fcc9eda3f3d223e6906ec511c2c13c6a376edef7
Leonardo Sanchez <leosanchezborges@gmail.com>
2022-03-02 19:19:21 -0300

    add: new route

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/json', (req, res) => {
+  res.send({
+    message: 'any response',
+    code: 42
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://127.0.0.1:${port}`)
 })
